b0f97eb0781de5d86669a229361ba15a2c091e3c
Oliver Carmont <olcarmontzaragoza@gmail.com>
2024-10-12 16:24:51 -0700

    fixed build failure

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,7 @@ import * as React from "react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
